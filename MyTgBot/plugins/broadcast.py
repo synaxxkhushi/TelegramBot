4b30a2c4fdfd68b471d9be032fa073162e9f094b
BARATH <115382856+BARATH-XD@users.noreply.github.com>
2024-03-03 20:19:04 +0530

    Update broadcast.py

--- MyTgBot/plugins/broadcast.py
+++ MyTgBot/plugins/broadcast.py
@@ -1,1 +1,107 @@
 #--------------------- Not Completed! -----------------
+
+import asyncio
+from pyrogram.types import *
+from pyrogram import filters
+from MyTgBot import bot
+from MyTgBot.database.chatsdb import get_served_chats
+from MyTgBot.database.usersdb import get_served_users
+
+@bot.on_message(filters.command(["broadcast", "gcast"], ["/", ".", "!", "?"])) 
+async def broadcast_message_nopin(_, message):
+    if not message.reply_to_message:
+        pass
+    else:
+        x = message.reply_to_message.message_id
+        y = message.chat.id
+        sent = 0
+        chats = []
+        schats = await get_served_chats()
+        for chat in schats:
+            chats.append(int(chat["chat_id"]))
+        for i in chats:
+            try:
+                m = await bot.forward_messages(i, y, x)
+                await asyncio.sleep(0.3)
+                sent += 1
+            except Exception:
+                pass
+        await message.reply_text(f"✅ Broadcast complete in {sent} Group.")
+        return
+    if len(message.command) < 2:
+        await message.reply_text(
+            "**usage**:\n\n/broadcast (`message`) or (`reply to message`)"
+        )
+        return
+    text = message.text.split(None, 1)[1]
+    sent = 0
+    chats = []
+    schats = await get_served_chats()
+    for chat in schats:
+        chats.append(int(chat["chat_id"]))
+    for i in chats:
+        try:
+            m = await bot.send_message(i, text=text)
+            await asyncio.sleep(0.3)
+            sent += 1
+        except Exception:
+            pass
+    await message.reply_text(f"✅ Broadcast complete in {sent} Group.")
+
+
+@bot.on_message(filters.command("broadcast_pin", ["/", "!", "?", "."]))
+async def broadcast_message_pin(_, message):
+    if not message.reply_to_message:
+        pass
+    else:
+        x = message.reply_to_message.message_id
+        y = message.chat.id
+        sent = 0
+        pin = 0
+        chats = []
+        schats = await get_served_chats()
+        for chat in schats:
+            chats.append(int(chat["chat_id"]))
+        for i in chats:
+            try:
+                m = await bot.forward_messages(i, y, x)
+                try:
+                    await m.pin(disable_notification=True)
+                    pin += 1
+                except Exception:
+                    pass
+                await asyncio.sleep(0.3)
+                sent += 1
+            except Exception:
+                pass
+        await message.reply_text(
+            f"✅ Broadcast complete in {sent} Group.\n📌 Sent with {pin} chat pins."
+        )
+        return
+    if len(message.command) < 2:
+        await message.reply_text(
+            "**usage**:\n\n/broadcast_pin (`message`) or (`reply to message`)"
+        )
+        return
+    text = message.text.split(None, 1)[1]
+    sent = 0
+    pin = 0
+    chats = []
+    schats = await get_served_chats()
+    for chat in schats:
+        chats.append(int(chat["chat_id"]))
+    for i in chats:
+        try:
+            m = await bot.send_message(i, text=text)
+            try:
+                await m.pin(disable_notification=True)
+                pin += 1
+            except Exception:
+                pass
+            await asyncio.sleep(0.3)
+            sent += 1
+        except Exception:
+            pass
+    await message.reply_text(
+        f"✅ Broadcast completed in {sent} Group.\n📌 Sent with {pin} chat pins."
+    )
