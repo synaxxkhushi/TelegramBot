0e69aa1344acce16c7abeec89a539815a475f958
BARATH <115382856+BARATH-XD@users.noreply.github.com>
2024-03-03 09:50:27 +0530

    Update ping.py

--- MyTgBot/plugins/ping.py
+++ MyTgBot/plugins/ping.py
@@ -5,7 +5,7 @@ from MyTgBot import bot, start, version
 from MyTgBot.help.readable_time import get_readable_time
 
 
-@bot.on_message(filters.command("ping"))
+@bot.on_message(filters.command("ping", ["/", ".", "?", "!"]))
 async def ping(_, message):
     currentTime = get_readable_time(time.time() - start)
     start_t = time.time()
