e5a61a3eacc47984970c24ff456a44c713c8d263
BARATH <115382856+BARATH-XD@users.noreply.github.com>
2024-03-09 17:03:48 +0530

    Update broadcast.py

--- MyTgBot/plugins/broadcast.py
+++ MyTgBot/plugins/broadcast.py
@@ -2,7 +2,7 @@
 
 import asyncio
 from pyrogram.types import Message
-from pyrogram import filters, Message
+from pyrogram import filters
 from MyTgBot import bot
 from MyTgBot.database.chatsdb import get_served_chats
 from MyTgBot.database.usersdb import get_served_users
