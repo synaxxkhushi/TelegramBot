f054e188a4bd0e1f6729a2bc94f4811f5c0661f1
BARATH <115382856+BARATH-XD@users.noreply.github.com>
2024-03-21 12:23:22 +0530

    Update broadcast.py

--- MyTgBot/plugins/broadcast.py
+++ MyTgBot/plugins/broadcast.py
@@ -48,36 +48,6 @@ async def bcast(_, m : Message):
 
     await lel.edit(f"✅ Successfull to sent `{success}` users.\n❌ Faild to sent `{failed}` users.\n👾 Found `{blocked}` Blocked users. \n👻 Found `{deactivated}` Deactivated users.")
 
-@bot.on_message(filters.command(["fcast", "forwardcast"], ["/", "?", "!", "."]))
-async def fcast(_, m : Message):
-    if m.from_user.id !=1666544436:
-         return await m.reply_text("`You Don't Have Enough Rights To Run This!`")
-    allusers = users
-    lel = await m.reply_text("`⚡️ Processing...`")
-    success = 0
-    failed = 0
-    deactivated = 0
-    blocked = 0
-    for usrs in allusers.find():
-        try:
-            userid = usrs["user_id"]
-            #print(int(userid))
-            if m.reply_to_message:
-                await m.reply_to_message.forward(int(userid))
-            success +=1
-        except FloodWait as ex:
-            await asyncio.sleep(ex.value)
-        except errors.InputUserDeactivated:
-            deactivated +=1
-            remove_user(userid)
-        except errors.UserIsBlocked:
-            blocked +=1
-        except Exception as e:
-            print(e)
-            failed +=1
-
-    await lel.edit(f"✅ Successfull to sent `{success}` users.\n❌ Faild to sent `{failed}` users.\n👾 Found `{blocked}` Blocked users. \n👻 Found `{deactivated}` Deactivated users.")
-
 @bot.on_message(filters.command(["gcast", "groupcast"], ["/", "?", "!", "."]))
 async def gcast(_, m : Message):
     if m.from_user.id !=1666544436:
