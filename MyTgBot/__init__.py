0c6893e7450572042986aa2502dbced7d568a4e1
BARATH <115382856+BARATH-XD@users.noreply.github.com>
2024-03-23 10:43:31 +0530

    Update __init__.py

--- MyTgBot/__init__.py
+++ MyTgBot/__init__.py
@@ -1,5 +1,5 @@
 from pyrogram import Client
-from pymongo import MongoClient
+from pymongo import MongoClient, errors as Mongoerrors
 import os
 import time
 
@@ -13,6 +13,12 @@ bot_token = os.getenv("TOKEN")
 MONGO = "mongodb+srv://kora3244:jNtUZleBWM71f1pC@haremdb.qxtdvdh.mongodb.net/?retryWrites=true&w=majority"
 mongo = MongoClient(MONGO)
 mongodb = mongo.BOT
+
+try:
+   mongodb.server_info()
+except Mongoerrors.ConnectionFailure:
+     print("Connection failure, INVALID MONGO DB URL!")
+     sys.exit()
 
 
 bot = Client("MyTgBot", 
