19faa8a94435b0de511312bb96b473e05d11600c
BARATH <115382856+BARATH-XD@users.noreply.github.com>
2024-03-30 09:27:40 +0530

    Update purge.py

--- MyTgBot/plugins/purge.py
+++ MyTgBot/plugins/purge.py
@@ -7,6 +7,7 @@ from MyTgBot import bot
 
 @bot.on_message(filters.command("purge", ["/", ".", "?", "!"]))
 async def purge(_, m):
+    reply = m.reply_to_message
     chat = m.chat
     user = m.from_user
     user_stats = await bot.get_chat_member(chat.id, user.id)
@@ -14,13 +15,13 @@ async def purge(_, m):
     if not bot_stats.privileges:
          return await m.reply_text("Make Me Admin REEE!!")
     if not user_stats.privileges:
-           return await m.reply_text("Only Admins are allowed to use this command!")    
+         return await m.reply_text("Only Admins are allowed to use this command!")    
     if not reply:
-           return  await m.reply_text("reply to message for deleting")
+         return  await m.reply_text("reply to message for deleting")
     if not bot_stats.privileges.can_delete_messages:
-           return await m.reply_text("**I'm missing the permission of**:\n`can_delete_messages`")
+         return await m.reply_text("**I'm missing the permission of**:\n`can_delete_messages`")
     if not user_stats.privileges.can_delete_messages:
-           return await m.reply_text("**your are missing the permission of**:\n`can_delete_messages`")
+         return await m.reply_text("**your are missing the permission of**:\n`can_delete_messages`")
     if m.chat.type != ChatType.SUPERGROUP:
         await m.reply_text("Cannot Purge Messages Here, Upgrade Your Group To Supergroup")
         return
